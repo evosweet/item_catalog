0512fed644dab4c123ce5beb63d9f71d40872e7b
Rayon Hunte <evosweet@gmail.com>
2017-02-12 14:11:09 -0400

    updates

--- puppy_query.py
+++ puppy_query.py
@@ -15,7 +15,8 @@ def get_puppyAlpha():
         print puppy.name + "\n"
         print puppy.dateOfBirth
 
-
+def less_six():
+    puppies = SESS.query(Puppy).order_by(asc(Puppy.name)).all()
 
 
 get_puppyAlpha()
